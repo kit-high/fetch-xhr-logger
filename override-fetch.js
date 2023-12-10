5851c1d79765968c412bad4bebe18eddad98cfb7
kit-high <123807900+kit-high@users.noreply.github.com>
2023-12-10 15:59:20 +0900

    Update override-fetch.js

--- override-fetch.js
+++ override-fetch.js
@@ -1,5 +1,18 @@
 const originalFetch = window.fetch;
 window.fetch = function (...args) {
-  console.log("Fetch request:", args);
-  return originalFetch.apply(this, args);
+  if (args[0].includes("api.example.com")) {
+    console.log("Fetch request:", args);
+
+    return originalFetch.apply(this, args).then(response => {
+        const responseClone = response.clone();
+
+        responseClone.json().then(json => {
+            console.log(`Fetch response for ${args[0]}:`, json);
+        }).catch(error => {
+            console.error("Error in reading response body as JSON:", error);
+        });
+
+        return response;
+    });
+  }
 };
